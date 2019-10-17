04fab77a641675d6669b5a8cc1f0206a8af1f58f
Miuywu <859@holbertonschool.com>
2019-10-17 06:27:43 +0000

    student class +json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Student:
+    """
+    student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Init
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        get dict
+        """
+        return self.__dict__
