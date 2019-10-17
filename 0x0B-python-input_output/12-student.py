6087f8b20a0b3aedd0b760ae357b078abf52b7b2
Miuywu <859@holbertonschool.com>
2019-10-17 06:36:55 +0000

    custom dict

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+class Student:
+    """
+    student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Init
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        get dict
+        """
+        if attrs is None:
+            return self.__dict__
+        f_dict = {}
+        for ki in attrs:
+            if ki in self.__dict__.keys():
+                f_dict[ki] = self.__dict__[ki]
+        return f_dict
