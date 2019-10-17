33cb96df853fc569eeca98627df9782c6a923fc9
Miuywu <859@holbertonschool.com>
2019-10-17 06:39:32 +0000

    setattr

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Student:
+    """
+    student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Init
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        get dict
+        """
+        if attrs is None:
+            return self.__dict__
+        f_dict = {}
+        for ki in attrs:
+            if ki in self.__dict__.keys():
+                f_dict[ki] = self.__dict__[ki]
+        return f_dict
+
+    def reload_from_json(self, json):
+        """
+        reloading
+        """
+        for ki in json:
+            setattr(self, ki, json[ki])
