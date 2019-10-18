33e7f41ddbb60656949163ad45971ec9eadfbb4b
Miuywu <859@holbertonschool.com>
2019-10-18 03:16:50 +0000

    added documentation

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    def area(self):
-        raise Exception("area() is not implemented")
+"""create class square from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
 
-    def integer_validator(self, name, value):
-        """validates value"""
-
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value  <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-class Rectangle(BaseGeometry):
-    """Rectangle inherited from BaseGeometry"""
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """area"""
-           return self.__width * self.__height
-
-    def __str__(self):
-        """[Rectangle] h/w"""
-        return "[Rectangle] " + "{}/{}".format(self.__width, self.__height)
 
 class Square(Rectangle):
     """Square inherits Rectangle"""
     def __init__(self, size):
+        """init square"""
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """area of square"""
