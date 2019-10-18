df60b6b32c53f8152dadf0f9c2e7d093837f9254
Miuywu <859@holbertonschool.com>
2019-10-18 03:26:33 +0000

    added newline between functions

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,6 +7,7 @@ class MyInt(int):
     def __ne__(self, value):
         """"!= -> =="""
         return super().__eq__(value)
+
     def __eq__(self, value):
         """== -> !="""
         return super().__ne__(value)
