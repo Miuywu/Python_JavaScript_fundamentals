0bb9f8721ee8f82a97f7736c3fa5f52337157ca4
Miuywu <859@holbertonschool.com>
2019-10-22 21:10:29 +0000

    all functions working

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Base Class"""
 import json
+from os import path
 
 
 class Base:
@@ -16,3 +18,61 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """list_dictionaries to JSON"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return '[]'
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """list to JSON to file"""
+
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w') as f:
+            if list_objs is None:
+                f.write(cls.to_json_string([]))
+            else:
+                l = [elem.to_dictionary() for elem in list_objs]
+                f.write(cls.to_json_string(l))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string:
+        json_string is a string representing a list of dictionaries
+        """
+
+        if json_string is None or json_string == '':
+            return []
+        return json.loads(json_string)
+
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set
+        """
+
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        cls.update(dummy, **dictionary)
+        return dummy
+
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        that returns a list of instances
+        """
+
+        filename = cls.__name__ + '.json'
+        if path.isfile(filename):
+            with open(filename, 'r') as f:
+                dictionary = json.load(f)
+            return [cls.create(**obj) for obj in dictionary]
+        return []
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Class Square"""
 from models.rectangle import Rectangle
 
 
