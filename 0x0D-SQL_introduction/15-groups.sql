bee60b02853a88b8211a93076f102e1f260b2313
Miuywu <859@holbertonschool.com>
2019-11-26 01:15:02 +0000

    syntax

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,10 +1,13 @@
 -- script that lists the number of records with the same score in the table
 -- second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT COUNT
-    score as number
+SELECT
+    score,
+COUNT(score)
+    AS number
 FROM
     second_table
 GROUP BY
     score
-number
+ORDER BY
+    score
 DESC;
