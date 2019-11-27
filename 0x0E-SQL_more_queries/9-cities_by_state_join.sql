b9fe646084b9fd9e811bdf4a591a00bb91a5d3fa
Miuywu <859@holbertonschool.com>
2019-11-27 02:03:05 +0000

    lists CA cities database records

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,14 @@
+-- script that lists all cities contained in the database hbtn_0d_usa
+SELECT
+    cities.id
+AS
+    id, cities.name, states.name
+AS
+    name
+FROM
+    states, cities
+WHERE
+    cities.state_id = states.id
+ORDER BY
+    cities.id
+ASC;
