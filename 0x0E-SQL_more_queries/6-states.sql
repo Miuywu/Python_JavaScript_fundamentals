d7eab0dc63b8f31427b2d439ddb9f8a5c718b977
Miuywu <859@holbertonschool.com>
2019-11-27 01:32:04 +0000

    create database

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- script that creates the database hbtn_0c_0 in your MySQL server.
+CREATE DATABASE IF NOT EXISTS
+    hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS
+    hbtn_0d_usa.states
+    (id INT PRIMARY KEY UNIQUE AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
