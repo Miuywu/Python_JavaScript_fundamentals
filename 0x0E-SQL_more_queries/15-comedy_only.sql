8334324aced604291ca60aee62b95a9ea65be579
Miuywu <859@holbertonschool.com>
2019-11-27 03:37:02 +0000

    lists comedy shows from tv database

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,18 @@
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT
+    title
+FROM
+    tv_shows
+JOIN
+    tv_show_genres
+ON
+    show_id = tv_shows.id
+JOIN
+    tv_genres
+ON
+    genre_id = tv_genres.id
+WHERE
+    name = "Comedy"
+ORDER BY
+    title
+ASC;
