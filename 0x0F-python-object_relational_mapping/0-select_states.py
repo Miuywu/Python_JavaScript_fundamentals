3ab93c5297d172f8e60a4044f7c26f6bd75d8e94
Miuywu <859@holbertonschool.com>
2019-12-15 23:15:27 +0000

    task 0 print table from mysql db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect
+    (
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        db=argv[3]
+    )
+    curs = database.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    a = curs.fetchall()
+    for row in a:
+        print(row)
+    curs.close()
+    database.close()
