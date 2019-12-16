8106ff07600af86e94b3e3d37cdedf8031d00574
Miuywu <859@holbertonschool.com>
2019-12-16 07:58:59 +0000

    new name

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa """
+
+import MySQLdb
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """ if comment """
+
+    engine = create_engine("mysql+mysqldb://" +
+                           "{}:{}@localhost/{}".format(sys.argv[1],
+                                                       sys.argv[2],
+                                                       sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    entry = session.query(State).filter(State.id == 2).first()
+    entry.name = "New Mexico"
+    session.commit()
+    session.close()
