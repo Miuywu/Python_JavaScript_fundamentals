d94bb7391d527af865b12f71b00fc28ec598ff3a
Miuywu <859@holbertonschool.com>
2019-12-16 00:49:27 +0000

    lists starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""holds one function"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """lists all states from the database hbtn_0e_usa"""
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        db=argv[3])
+
+    curs = database.cursor()
+    curs.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+
+    a = curs.fetchall()
+    for row in a:
+        print(row)
+    curs.close()
+    database.close()
