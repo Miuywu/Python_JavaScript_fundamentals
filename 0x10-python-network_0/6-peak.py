37a82f39ba110f78212ad06e4298395448c0c798
Miuywu <859@holbertonschool.com>
2020-01-09 19:15:19 +0000

    pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def find_peak(list_of_integers):
     if not list_of_integers:
         return
@@ -8,15 +9,19 @@ def find_peak(list_of_integers):
     while fin == 0:
         if curr == 0 and list_of_integers[curr + 1] < list_of_integers[curr]:
             return list_of_integers[curr]
-        elif curr == len(list_of_integers) - 1 and list_of_integers[curr - 1] < list_of_integers[curr]:
+        elif curr == len(list_of_integers) - 1
+        and list_of_integers[curr - 1] < list_of_integers[curr]:
             return list_of_integers[curr]
-        elif list_of_integers[curr - 1] < list_of_integers[curr] and list_of_integers[curr + 1] < list_of_integers[curr]:
+        elif list_of_integers[curr - 1] < list_of_integers[curr]
+        and list_of_integers[curr + 1] < list_of_integers[curr]:
             return list_of_integers[curr]
-        elif list_of_integers[curr + 1] and list_of_integers[curr + 1] > list_of_integers[curr]:
+        elif list_of_integers[curr + 1]
+        and list_of_integers[curr + 1] > list_of_integers[curr]:
             curr += 1
             if curr == list_of_integers:
                 return None
-        elif list_of_integers[curr - 1] and list_of_integers[curr - 1] > list_of_integers[curr]:
+        elif list_of_integers[curr - 1]
+        and list_of_integers[curr - 1] > list_of_integers[curr]:
             curr -= 1
             if curr == -1:
                 return None
