06a8d00dc146b4912323ec7f136992f1bc80b913
Miuywu <859@holbertonschool.com>
2020-01-13 21:15:09 +0000

    tabs?

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,5 +3,5 @@ import urllib.request
 import sys
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-        dict = response.info()
-        print(dict.get('X-Request-Id'))
+    dict = response.info()
+    print(dict.get('X-Request-Id'))
