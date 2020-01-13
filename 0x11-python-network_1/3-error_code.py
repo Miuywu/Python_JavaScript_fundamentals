b21b421fd19bfcee8410d4010eb451c367a5a674
Miuywu <859@holbertonschool.com>
2020-01-13 22:10:56 +0000

    details

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,11 +2,12 @@
 """ tries to get a url response, if error catches and prints """
 import urllib.request
 import sys
-import urllib.parse
 
 if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
     try:
-        urllib.request.urlopen(req)
-    except urllib.error.URLError as e:
-        print(e.reason)
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
