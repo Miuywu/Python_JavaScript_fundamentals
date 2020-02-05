75b0d350d9fbe5f2b194eb735e984271f8a5f78b
Miuywu <859@holbertonschool.com>
2020-02-04 21:15:48 -0800

    high level magic

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-let max = 0;
-let max2 = 0;
-for (let b = 2; b < process.argv.length; b++) {
-  if (parseInt(process.argv[b]) > max) {
-    max2 = max;
-    max = parseInt(process.argv[b]);
-  }
+if (process.argv.length <= 2) {
+  console.log(0);
+} else {
+  console.log(process.argv.slice(2).sort((a, b) => a - b)[process.argv.length - 4]);
 }
-console.log(max2);
