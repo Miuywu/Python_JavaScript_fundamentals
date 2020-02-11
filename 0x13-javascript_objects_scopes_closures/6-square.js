3ebdb23bbca5f01ccc935d18a3f04d090b82d70b
Miuywu <859@holbertonschool.com>
2020-02-10 18:13:35 -0800

    import file in js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,43 +1,14 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
-      return;
-    }
-    this.width = w;
-    this.height = h;
-  }
+const fiver = require('./5-square');
 
-  print () {
-    for (let a = 0; a < this.height; a++) {
-      let line = '';
-      for (let b = 0; b < this.width; b++) {
-        line += 'X';
-      }
-      console.log(line);
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends fiver {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
+      this.print();
     } else {
       for (let a = 0; a < this.height; a++) {
         let line = '';
